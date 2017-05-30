cbbdf4aba1de00ba40db028461e2ce605232a262
aaroncadillac <aaron.hmaya@gmail.com>
2017-05-30 14:50:11 -0500

    test1

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ const fs = require('fs'),
       google = require('googleapis'),
       googleAuth = require('google-auth-library'),
       SCOPES = ['https://www.googleapis.com/auth/spreadsheets'],
-      AWS = require('aws-sdk');
-// Path for tokens
-      TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + '/.credentials/',
-      TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs.json',
+      AWS = require('aws-sdk'),
+      S3 = AWS.S3();
 
-      IAM = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + '/.google/secret/';
+function getS3Obj(key, callback){
+  var paramsS3Object = {
+    Bucket: 'coconutt-website', /* required */
+    Key: key, /* required */
+    ResponseContentEncoding: 'utf8',
+    ResponseContentType: 'application/json',
+    SSECustomerAlgorithm: 'AES256'
+  };
 
+  s3.getObject(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else data;         // successful response
+  });
+}
 
-/**
- * Demonstrates a simple HTTP endpoint using API Gateway. You have full
- * access to the request and response payload, including headers and
- * status code.
- *
- * To scan a DynamoDB table, make a GET request with the TableName as a
- * query string parameter. To put, update, or delete an item, make a POST,
- * PUT, or DELETE request respectively, passing in the payload to the
- * DynamoDB API as a JSON body.
- */
 exports.handler = (event, context, callback) => {
     //console.log('Received event:', JSON.stringify(event, null, 2));
 
@@ -33,41 +33,19 @@ exports.handler = (event, context, callback) => {
     });
 
     switch (event.httpMethod) {
-        case 'DELETE':
-            dynamo.deleteItem(JSON.parse(event.body), done);
-            break;
-        case 'GET':
-            dynamo.scan({ TableName: event.queryStringParameters.TableName }, done);
-            break;
         case 'POST':
-            dynamo.putItem(JSON.parse(event.body), done);
-            break;
-        case 'PUT':
-            dynamo.updateItem(JSON.parse(event.body), done);
+          // Load client
+          getS3Obj('gssectret,json', function (data = null) {
+            if (data) {
+              authorize(data, createRequest, event);
+            }
+          });
             break;
         default:
             done(new Error(`Unsupported method "${event.httpMethod}"`));
     }
 };
 
-// Load client
-fs.readFile(IAM + 'gssecret.json', function processClientSecrets(err, content) {
-  if (err) {
-    console.log('Error loading client secret file: ' + err);
-    return;
-  }
-  // Authorize a client with the loaded credentials, then call the
-  // Google Sheets API.
-  authorize(JSON.parse(content), createRequest);
-});
-
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- *
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
 function authorize(credentials, callback) {
   var clientSecret = credentials.installed.client_secret;
   var clientId = credentials.installed.client_id;
@@ -75,84 +53,32 @@ function authorize(credentials, callback) {
   var auth = new googleAuth();
   var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
 
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, function(err, token) {
-    if (err) {
-      getNewToken(oauth2Client, callback);
-    } else {
+  getS3Obj('sheets.googleapis.com-nodejs.json', function (data = null) {
+    if (data) {
       oauth2Client.credentials = JSON.parse(token);
-      callback(oauth2Client);
+      callback(oauth2Client,event);
     }
   });
 }
 
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- *
- * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback to call with the authorized
- *     client.
- */
-function getNewToken(oauth2Client, callback) {
-  var authUrl = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES
-  });
-  console.log('Authorize this app by visiting this url: ', authUrl);
-  var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  rl.question('Enter the code from that page here: ', function(code) {
-    rl.close();
-    oauth2Client.getToken(code, function(err, token) {
-      if (err) {
-        console.log('Error while trying to retrieve access token', err);
-        return;
-      }
-      oauth2Client.credentials = token;
-      storeToken(token);
-      callback(oauth2Client);
-    });
-  });
-}
-
-/**
- * Store token to disk be used in later program executions.
- *
- * @param {Object} token The token to store to disk.
- */
-function storeToken(token) {
-  try {
-    fs.mkdirSync(TOKEN_DIR);
-  } catch (err) {
-    if (err.code != 'EEXIST') {
-      throw err;
-    }
-  }
-  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-  console.log('Token stored to ' + TOKEN_PATH);
-}
-
-function createRequest(auth) {
-  request = {
-    auth: auth
-  };
+function createRequest(auth,event) {
   
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+  var resourceArray = [];
+  
+  var request = {
+    auth: auth,
+    spreadsheetId: '1bhXbigMkNyTgKFVePZIwP5VZE1hN0XcvTRdeFdUSUdo'
+  };
 
-  rl.question('Enter a valid SpreadSheetID (You must have w/r permissions): ', function(code) {
-    request.spreadsheetId = code;
-    rl.question("Enter path for JSON: ", function(path) {
-      rl.close();
-      request.resource = JSON.parse(fs.readFileSync(path, 'utf8'));
-      addValues(request)
-    });
-  });
+  for (key in event) {
+    resource.push(event.key);
+  }
+
+  request.reosurce = [
+    resourceArray;
+  ]
+
+  addValues(requestJSON)
 }
 
 function addValues(requestJSON) {
@@ -172,6 +98,4 @@ function addValues(requestJSON) {
     }
     console.log(response);
   })
-
-  //console.log(sendObject.resource)
 }
